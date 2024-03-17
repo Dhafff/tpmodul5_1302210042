686089c3a2d9ef7bc204b307bd73ab3e492b5100
Dhafff <dhafgoes@student.telkomuniversity.ac.id>
2024-03-18 05:55:24 +0700

    menambahkan class DataGeneric

--- Program.cs
+++ Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        HaloGeneric.SapaUser<string>("Dhafa");
+        Console.WriteLine();
+        DataGeneric<int> DataNIM = new DataGeneric<int>(1302210042);
+        Console.WriteLine(DataNIM.PrintData(InputData));
+    }
+} 
+
+public class HaloGeneric
+{
+    public static void SapaUser<T>(T InputanUser)
+    {
+        Console.WriteLine("Halo user " + InputanUser);
+    }
+}
+
+public class DataGeneric<T>
+{
+    private T data;
+
+    public DataGeneric(T InputData)
+    {
+        this.data = InputData;
+    }
+
+    public static void PrintData(T InputData)
+    {
+        Console.WriteLine("Data yang tersimpan adalah: " + InputData);
+    }
+}
